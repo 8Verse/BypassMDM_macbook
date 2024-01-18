14e8824de3440bbd713fd8deb7003f2cd9df669a
8Verse <96404249+8Verse@users.noreply.github.com>
2024-01-18 15:18:48 +0700

    Update fastbypass.sh

--- fastbypass.sh
+++ fastbypass.sh
@@ -18,14 +18,14 @@ dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UserShel
 dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" RealName "$realName"
 dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" RealName "$realName"
 dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UniqueID "501"
-dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" PrimaryGroupID "20"
+dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" PrimaryGroupID 20
 mkdir "/Volumes/MacOS/Users/$username"
 dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" NFSHomeDirectory "/Users/$username"
 dscl -f "$dscl_path" localhost -passwd "/Local/Default/Users/$username" "$passw"
 dscl -f "$dscl_path" localhost -append "/Local/Default/Groups/admin" GroupMembership $username
-echo "0.0.0.0 deviceenrollment.apple.com" >>/Volumes/MacOS/etc/hosts
-echo "0.0.0.0 mdmenrollment.apple.com" >>/Volumes/MacOS/etc/hosts
-echo "0.0.0.0 iprofiles.apple.com" >>/Volumes/MacOS/etc/hosts
+sh -c 'echo "0.0.0.0 deviceenrollment.apple.com" >>/etc/hosts'
+sh -c 'echo "0.0.0.0 mdmenrollment.apple.com" >>/etc/hosts'
+sh -c 'echo "0.0.0.0 iprofiles.apple.com" >>/etc/hosts'
 echo -e "Chặn host thành công"
 # echo "Remove config profile"
 touch /Volumes/MacOS/var/db/.AppleSetupDone
